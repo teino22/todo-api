e885c75c288f6e632bddf486174075b81783d97f
teino22 <matteo.games2@gmail.com>
2016-01-18 12:26:05 +0100

    Added todos

--- server.js
+++ server.js
@@ -2,8 +2,26 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+var todos = [{
+	id: 1,
+	description: 'something to do',
+	completed : false
+}, {
+	id: 2,
+	description: 'go to market',
+	completed : false
+}];
+
 app.get('/', function (req, res){
 	res.send('Todo API Root');
+});
+
+app.get('/todos', function (req, res){
+	res.send(todos);
+});
+
+app.get('/todos/:id', function (req, res){
+	res.send(todos[id]);
 });
 
 app.listen(PORT, function() {

93e56e740f378bb4aa9ccf1b2ad5366d94fc992e
teino22 <matteo.games2@gmail.com>
2016-01-18 14:40:02 +0100

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
+var app = express();
 var PORT = process.env.PORT || 3000;
 
 var todos = [];
 var todoNextId = 1;
 
 app.use(bodyParser.json());
-
 
 app.get('/', function (req, res){
 	res.send('Todo API Root');
@@ -19,12 +20,7 @@ app.get('/todos', function (req, res){
 
 app.get('/todos/:id', function (req, res){
 	var todoId = parseInt(req.params.id,10);
-	var matchedTodo;
-	todos.forEach(function (todo){
-		if (todo.id === todoId){
-			matchedTodo = todo;	
-		}
-	})
+	var matchedTodo = _.findWhere(todos, {id:todoId});
 	if (matchedTodo){
 		res.json(matchedTodo);
 	}
@@ -34,7 +30,11 @@ app.get('/todos/:id', function (req, res){
 });
 
 app.post ('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body,'description','completed');
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || _.isEmpty(body.description.trim())){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 	todos.push(body);
 	res.json(body);

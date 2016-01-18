b384580553d0eaccbea8b05408af341967016c5f
teino22 <matteo.games2@gmail.com>
2016-01-18 15:56:49 +0100

    Add variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+// 	name: 'Matteo',
+// 	age: 21
+// };
+
+// function updatePerson(obj){
+// 	obj.age= 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+var grades = [15, 18];
+
+function addGrades(grades){
+	grades.push(22);
+	debugger;
+	// grades = [11,22,33];
+}
+
+addGrades(grades);
+
+console.log(grades);
+

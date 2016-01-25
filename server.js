ceeead41e99af218b209c97304b6955d31aacd96
teino22 <matteo.games2@gmail.com>
2016-01-25 11:20:45 +0100

    Hash and salt password

--- server.js
+++ server.js
@@ -116,13 +116,15 @@ app.put('/todos/:id', function(req, res) {
 app.post('/users', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 	db.user.create(body).then(function(user) {
-		res.json(user.toJSON());
+		res.json(user.toPublicJSON());
 	}, function(e) {
 		res.status(400).send(e);
 	})
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync(
+	// {force: true}
+).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});

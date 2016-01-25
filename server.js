7eb3c26592723770955f7bb0fbae1819a0665f4a
teino22 <matteo.games2@gmail.com>
2016-01-25 15:06:15 +0100

    Added DELETE /users/login

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 
 	db.todo.findOne({
-		where:{
+		where: {
 			id: todoId,
 			userId: req.user.get('id')
 		}
@@ -144,24 +144,34 @@ app.post('/users', function(req, res) {
 
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
-		if (token) {
-			res.header('Auth', token).json(user.toPublicJSON());
-		} else {
-			res.status(401).send();
-		}
+		userInstance = user;
+		return db.token.create({
+			token: token
+		});
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
 
-	}, function(e) {
+	}).catch(function(e) {
 		res.status(401).send();
 	});
 });
 
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function (){
+		res.status(204).send();
+	}, function (){
+		res.status(500).send();
+	});
+});
 
-db.sequelize.sync(
-	 {force: true}
-).then(function() {
+
+db.sequelize.sync({
+	force: true
+}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
